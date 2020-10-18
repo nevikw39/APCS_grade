6255e1b383f1866ac9a115914820e4afacba43c4
nevikw39 <nevikw39@icloud.com>
2020-10-18 21:26:40 +0800

    Add comments on main.py

--- main.py
+++ main.py
@@ -8,22 +8,28 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def wk(x: str, y):
+def wk(x: str, y: str):
+    """
+    Get the value of the hidden form element.
+    """
     return bs(y, 'html.parser').select_one(f'input[name="wk_{x}"]').attrs["value"]
 
 
 def get_subject():
+    """
+    Get the subject of the latest email in inbox.
+    """
     _, idx = imap.select("INBOX")
     _, msg = imap.fetch(idx[0].decode(), "(RFC822)")
     return email.header.decode_header(email.message_from_bytes(msg[0][1])["Subject"])[0][0].decode()
 
 
-try:
+if os.path.exists("config.pickle"):
     with open("config.pickle", 'rb') as p:
         ID = pickle.load(p)
         EMAIL = pickle.load(p)
         PASSW = pickle.load(p)
-except FileNotFoundError:
+else
     ID = input("ID:\n")
     EMAIL = input("\nEmail:\n")
     PASSW = input("\nPassword:\n")
@@ -32,15 +38,18 @@ except FileNotFoundError:
         pickle.dump(EMAIL, p)
         pickle.dump(PASSW, p)
 
+# First, post our ID and email, and wait the check number mail to our box.
 s = requests.session()
 r = s.get("https://webapi.apcs.csie.ntnu.edu.tw/APCS/Applygrade.do")
 r = s.post("https://webapi.apcs.csie.ntnu.edu.tw/APCS/Applygrade.do",
            {"wk_action": "a_getchecknum", "wk_token": wk("token", r.text), "wk_authnumber": ID, "wk_email": EMAIL})
 
+# Second, log in to our gmail and wait for the check number.
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 try:
     imap.login(EMAIL, PASSW)
 except imaplib.IMAP4.error:
+    # Only if you turn on "Less secure app access" can we read your emails via IMAP.
     print("IMAP log-in failed.\n\tPlease check if both your email and password is correct, and whether \"Less secure app access\" is on for your account.\n\tFor more detail, visit https://github.com/nevikw39/APCS_grade")
     if os.path.exists("config.pickle"):
         os.remove("config.pickle")
@@ -48,14 +57,15 @@ except imaplib.IMAP4.error:
 subject = ""
 while "大學程式設計先修檢測】驗證碼:" not in subject:
     subject = get_subject()
-    time.sleep(1)
+    time.sleep(1) # ``Patient you must have'' by Yoda
 num = subject[-5:]
 _, idx = imap.select("INBOX")
-imap.store(idx[0].decode(), '+FLAGS', '\\Deleted')
+imap.store(idx[0].decode(), '+FLAGS', '\\Deleted') # Delete the verifying email.
 imap.expunge()
 imap.close()
 imap.logout()
 
+# Finally, we have the check number and we can go back to the form
 r = s.get("https://webapi.apcs.csie.ntnu.edu.tw/APCS/Applygrade.do")
 r = s.post("https://webapi.apcs.csie.ntnu.edu.tw/APCS/Applygrade.do", {"wk_action": "a_validateCheckNum", "wk_token": wk(
     "token", r.text), "wk_authnumber": ID, "wk_email": EMAIL, "wk_testList": wk("testList", r.text), "wk_checknum": num})
